0e5f7726d1536c5848ff723a3d89d6df9577d78a
ibrahim yeryaran <ibrahimyeryaran93@gmail.com>
2025-02-16 01:42:54 +0300

    feat: queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,42 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require('../extensions/list-node.js');
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const newNode = new ListNode(value);
+    if (this.tail) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  dequeue() {
+    if (!this.head) {
+      return null;
+    }
+    const dequeuedValue = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return dequeuedValue;
+  }
+}
+
+module.exports = {
+  Queue
+};const { NotImplementedError } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
